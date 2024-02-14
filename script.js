a9b45bec3f60b22c272681c2de41ccc144be701c
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-15 02:19:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+let index = 0;
 function createTable() {
     //Write your code here
-  
+	const row = prompt("Input number of rows");
+	const column = prompt("Input number of columns");
+	const table = document.getElementById("myTable");
+	for(let i=index ; i < index + row ; i++){
+		let currRow = document.createElement("tr");
+		for(let j=0 ; j < column ; j++){
+			let currCol = document.createElement("td");
+			currCol.innerText = `Row-${i} Column-${j}`;
+			currRow.appendChild(currCol);
+		}
+		table.appendChild(currRow);
+	}
+	index = row;	
 }
